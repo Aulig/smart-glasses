6ff2d848819da84dfa2c57600caa7d13f93ff2e0
Aulig <janbunk87493@gmail.com>
2023-08-11 23:54:52 +0200

    log distances to thingspeak

---
+++ README.md
@@ -0,0 +1,3 @@
+# Setup
+- Create authentication.py file with the following variables
+  - THINGSPEAK_WRITE_API_KEY = "your thingspeak channel write api key"
--- main.py
+++ main.py
@@ -1,29 +1,56 @@
 import RPi.GPIO as GPIO
 import time
 
-try:
-    GPIO.setmode(GPIO.BOARD)
-    pinTrigger = 2 # or 3
-    pinEcho = 3 # or 5
+import requests as requests
 
-    GPIO.setup(pinTrigger, GPIO.OUT)
-    GPIO.setup(pinEcho, GPIO.IN)
+from authentication import THINGSPEAK_WRITE_API_KEY
 
-    GPIO.output(pinTrigger, GPIO.LOW)
-    GPIO.output(pinTrigger, GPIO.HIGH)
+SPEED_OF_SOUND = 34300  # cm/s
 
-    time.sleep(0.00001)
-    GPIO.output(pinTrigger, GPIO.LOW)
+# https://i.stack.imgur.com/yHddo.png
+DISTANCE_TRIGGER_PIN = 4
+DISTANCE_ECHO_PIN = 17
 
-    while GPIO.input(pinEcho) == 0:
-        pulseStartTime = time.time()
 
-    while GPIO.input(pinEcho) == 1:
-        pulseEndTime = time.time()
+def measure_distance():
+    try:
+        GPIO.setmode(GPIO.BCM)
 
-    pulseDuration = pulseEndTime - pulseStartTime
-    distance = round(pulseDuration * 17150, 2)
+        # distance measurement tutorial: https://linuxhint.com/measure_distance_raspberry_pi/
+        GPIO.setup(DISTANCE_TRIGGER_PIN, GPIO.OUT)
+        GPIO.setup(DISTANCE_ECHO_PIN, GPIO.IN)
 
-    print("Distance: %.2f cm" % (distance))
-finally:
-    GPIO.cleanup()
+        GPIO.output(DISTANCE_TRIGGER_PIN, GPIO.LOW)
+        GPIO.output(DISTANCE_TRIGGER_PIN, GPIO.HIGH)
+
+        time.sleep(0.00001)
+        GPIO.output(DISTANCE_TRIGGER_PIN, GPIO.LOW)
+
+        while GPIO.input(DISTANCE_ECHO_PIN) == 0:
+            time.sleep(0.00001)
+
+        pulse_start_time = time.time()
+
+        while GPIO.input(DISTANCE_ECHO_PIN) == 1:
+            time.sleep(0.00001)
+
+        pulse_end_time = time.time()
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = pulse_duration * SPEED_OF_SOUND / 2
+
+        print(f"Distance: {distance}cm")
+
+        requests.get("https://api.thingspeak.com/update",
+                     params={
+                         "field1": distance,
+                         "api_key": THINGSPEAK_WRITE_API_KEY}
+                     )
+    finally:
+        GPIO.cleanup()
+
+
+while True:
+    measure_distance()
+    # 15 seconds is the rate limit for ThingSpeak (free account)
+    time.sleep(15)
